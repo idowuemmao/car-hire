bf7f110186ae041679253e7404e07e16052ea43c
Idowu Emmanuel <idowuemmao@gmail.com>
2023-10-25 11:49:05 +0100

    Added the model search button

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -16,6 +16,7 @@ const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
 );
 const SearchBar = () => {
   const [manufacturer, setManufacturer] = useState("");
+  const [model, setModel] = useState("");
 
   const handleSearch = () => {
     console.log("grace");
@@ -23,17 +24,34 @@ const SearchBar = () => {
   return (
     <form
       onSubmit={handleSearch}
-      className="flex items-center justify-start max-sm:flex-col w-full relative max-sm:gap-4 max-w-3xl"
+      className="flex items-center justify-start max-sm:flex-col w-full relative gap-1 max-sm:gap-4 max-w-3xl"
     >
-      <div className="flex-1 max-sm:w-full flex justify-start items-center relative">
+      <div className="flex-1 max-sm:w-full flex justify-start items-center relative border-2">
         <SearchManufacturer
           manufacturer={manufacturer}
-          setManufacturer={() => {
-            console.log(SearchManufacturer);
-          }}
+          setManufacturer={setManufacturer}
         />
         <SearchButton otherClasses="sm:hidden" />
       </div>
+      <div className="flex-1 max-sm:w-full border-2 flex justify-start items-center relative">
+        <Image
+          src="/model-icon.png"
+          width={25}
+          height={25}
+          alt="car model"
+          className="absolute w-5 h-5 ml-4"
+        />
+        <input
+          type="text"
+          name="model"
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder="Tiguan"
+          className="w-full h-12 pl-12 p-4 bg-light-white rounded-r-full max-sm:rounded-full outline-none cursor-pointer text-sm"
+        />
+        <SearchButton otherClasses="sm:hidden" />
+      </div>
+      <SearchButton otherClasses="max-sm:hidden" />
     </form>
   );
 };
