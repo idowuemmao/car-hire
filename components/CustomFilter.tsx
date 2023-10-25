0614ee5514619fab3d50d821cffd21ea8ecf7748
Idowu Emmanuel <idowuemmao@gmail.com>
2023-10-25 17:36:32 +0100

    Updating the customfilter

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,17 +1,26 @@
 "use client";
 import { CustomFilterProps } from "@/types";
+import { updateSearchParams } from "@/utils";
 import { Listbox, Transition } from "@headlessui/react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import React, { Fragment, useState } from "react";
 
 const CustomFilter = ({ title, options }: CustomFilterProps) => {
+  const router = useRouter();
   const [selected, setSelected] = useState(options[0]);
+  const handleUpdateParams = (e: { title: string; value: string }) => {
+    const newPathName = updateSearchParams(title, e.value.toLowerCase());
+
+    router.push(newPathName);
+  };
   return (
     <div className="w-fit ">
       <Listbox
         value={selected}
         onChange={(e) => {
           setSelected(e);
+          handleUpdateParams(e);
         }}
       >
         <div className="relative w-fit z-10">
