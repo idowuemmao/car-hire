5d0d8888dffb0e2d2b5f59b5c79b126324f9ba14
Idowu Emmanuel <idowuemmao@gmail.com>
2023-10-25 12:21:23 +0100

    added functionality to the search button + routing

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import { SearchManufacturer } from ".";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
   <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
@@ -17,9 +18,33 @@ const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
 const SearchBar = () => {
   const [manufacturer, setManufacturer] = useState("");
   const [model, setModel] = useState("");
+  const router = useRouter();
 
-  const handleSearch = () => {
-    console.log("grace");
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); //to remove the default page refresh action once the form is submitted
+    //to check if we don't have a manufacturer or a model
+    if (manufacturer === "" && model === "") {
+      return alert("Please fill in the search bar");
+    }
+    updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase());
+  };
+  const updateSearchParams = (model: string, manufacturer: string) => {
+    const searchParams = new URLSearchParams(window.location.search);
+    if (model) {
+      searchParams.set("model", model);
+    } else {
+      searchParams.delete("model");
+    }
+    if (manufacturer) {
+      searchParams.set("manufacturer", manufacturer);
+    } else {
+      searchParams.delete("manufacturer");
+    }
+    const newPathName = `${
+      window.location.pathname
+    }?${searchParams.toString()}`;
+
+    router.push(newPathName);
   };
   return (
     <form
