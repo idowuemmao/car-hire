f7ed16df64c68fdfa9113cadc6fded2e73c491dc
Idowu Emmanuel <idowuemmao@gmail.com>
2023-10-25 16:55:05 +0100

    fixed thte bug in custom filter

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import React, { Fragment, useState } from "react";
 
 const CustomFilter = ({ title, options }: CustomFilterProps) => {
-  const [selected, setSelected] = useState("");
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div className="w-fit ">
       <Listbox
@@ -42,7 +42,15 @@ const CustomFilter = ({ title, options }: CustomFilterProps) => {
                     }`
                   }
                 >
-                  {({ selected }) => <span>{option.title}</span>}
+                  {({ selected }) => (
+                    <span
+                      className={`block truncate ${
+                        selected ? "font-medium" : "font-normal"
+                      }`}
+                    >
+                      {option.title}
+                    </span>
+                  )}
                 </Listbox.Option>
               ))}
             </Listbox.Options>
